655645979097993d95bd1d78bfc2dfc519c623f1
raldblox <raldblox@outlook.com>
2024-01-02 17:27:50 +0800

    init smart-assets npm package

---
+++ contracts/Hello.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// OnChainVision Contracts
+
+pragma solidity 0.8.19;
+
+contract Hello {
+    constructor() {}
+
+    function hello() public pure returns (string memory) {
+        return "hi there!";
+    }
+}
