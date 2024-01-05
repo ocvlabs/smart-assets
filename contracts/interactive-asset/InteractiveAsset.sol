4935be33dbcf40cc196b3a612aa86518f06929e8
raldblox <raldblox@outlook.com>
2024-01-06 06:16:21 +0800

    record asset name

--- contracts/interactive-asset/InteractiveAsset.sol
+++ contracts/interactive-asset/InteractiveAsset.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.9;
 import "./IInteractiveAsset.sol";
 
 /// @title Compose smart assets into interactive 'markup-based' smart asset
-contract InteractiveAsset {
+contract InteractiveAsset is IInteractiveAsset {
+    string _assetName;
     address private _creatorAddress;
     address private _styleAddress;
     address private _bodyAddress;
@@ -12,12 +13,14 @@ contract InteractiveAsset {
     address private _scriptAddress;
 
     constructor(
+        string memory assetName,
         address creatorAddress,
         address styleAddress,
         address bodyAddress,
         address settingAddress,
         address scriptAddress
     ) {
+        _assetName = assetName;
         _creatorAddress = creatorAddress;
         _styleAddress = styleAddress;
         _bodyAddress = bodyAddress;
@@ -25,16 +28,18 @@ contract InteractiveAsset {
         _scriptAddress = scriptAddress;
     }
 
-    function viewComposition()
+    function getComposition()
         public
         view
         returns (
+            string memory assetName,
             address styleAddress,
             address bodyAddress,
             address settingAddress,
             address scriptAddress
         )
     {
+        assetName = _assetName;
         styleAddress = _styleAddress;
         bodyAddress = _bodyAddress;
         settingAddress = _settingAddress;
