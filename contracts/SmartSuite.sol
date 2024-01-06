ca9c877f9fcb1ba48f39127d19e3e19b6be37078
raldblox <raldblox@outlook.com>
2024-01-06 10:57:17 +0800

    compiled all contracts into suite

---
+++ contracts/SmartSuite.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// OnChainVision Contracts
+
+pragma solidity ^0.8.20;
+
+import {AssetRegistry} from "./asset-manager/AssetRegistry.sol";
+import {TokenFactory, ITokenFactory} from "./asset-manager/TokenFactory.sol";
+import {SmartAsset, ISmartAsset} from "./smart-asset/SmartAsset.sol";
+import {InteractiveAsset, IInteractiveAsset} from "./interactive-asset/InteractiveAsset.sol";
