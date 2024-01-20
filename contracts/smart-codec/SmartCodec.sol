196b678ea787f1dc2cff0b4086dcbaba6b2538b8
raldblox <raldblox@outlook.com>
2024-01-20 16:19:00 +0800

    added constant markups in bytes

--- contracts/smart-codec/SmartCodec.sol
+++ contracts/smart-codec/SmartCodec.sol
@@ -5,6 +5,14 @@ import {Base64} from "./Base64.sol";
 
 /// @title Libraries/Utils for Encoding and Decoding Smart Assets
 library SmartCodec {
+    string internal constant MARKUP1 =
+        "PCFET0NUWVBFIGh0bWw+IDxodG1sIGxhbmc9ZW4+IDxoZWFkPiA8dGl0bGU+";
+    string internal constant MARKUP2 =
+        "PC90aXRsZT4gPG1ldGEgaHR0cC1lcXVpdj1jb250ZW50LXR5cGUgY29udGVudD0idGV4dC9odG1sOyBjaGFyc2V0PVVURi04Ij4gPG1ldGEgY2hhcnNldD1VVEYtOD48bWV0YSBodHRwLWVxdWl2PVgtVUEtQ29tcGF0aWJsZSBjb250ZW50PSJJRT1lZGdlIj4gPG1ldGEgbmFtZT12aWV3cG9ydCBjb250ZW50PSJ3aWR0aD1kZXZpY2Utd2lkdGgsaW5pdGlhbC1zY2FsZT0xIj4gPHN0eWxlPg==";
+    string internal constant MARKUP3 = "PC9zdHlsZT48L2hlYWQ+PGJvZHk+";
+    string internal constant MARKUP4 = "IDxzY3JpcHQgdHlwZT1tb2R1bGU+IA==";
+    string internal constant MARKUP5 = "IDwvc2NyaXB0PiA8L2JvZHk+IDwvaHRtbD4=";
+
     function encode64(string memory data) public pure returns (string memory) {
         return Base64.encode(bytes(data));
     }
@@ -35,29 +43,6 @@ library SmartCodec {
             );
     }
 
-    function encodeMarkup64(
-        string memory data
-    ) external pure returns (string memory) {
-        return
-            string(abi.encodePacked("data:text/html;base64,", encode64(data)));
-    }
-
-    function encodeMarkup64(
-        string memory title,
-        string memory style,
-        string memory body,
-        string memory setting,
-        string memory script
-    ) external pure returns (string memory) {
-        return
-            string(
-                abi.encodePacked(
-                    "data:text/html;base64,",
-                    encode64(encodeMarkup(title, style, body, setting, script))
-                )
-            );
-    }
-
     function encodeMetadata(
         string memory name,
         string memory description,
@@ -83,28 +68,65 @@ library SmartCodec {
             );
     }
 
+    function encodeMarkup64(
+        string memory data
+    ) external pure returns (string memory) {
+        return
+            string(abi.encodePacked("data:text/html;base64,", encode64(data)));
+    }
+
+    function encodeMarkup64(
+        string memory title,
+        string memory style,
+        string memory body,
+        string memory setting,
+        string memory script
+    ) external pure returns (string memory) {
+        string memory encodedMarkup = encodeMarkup(
+            title,
+            style,
+            body,
+            setting,
+            script
+        );
+        string memory base64Document = string(
+            abi.encodePacked("data:text/html;base64,", encode64(encodedMarkup))
+        );
+        return base64Document;
+    }
+
     function encodeMarkup(
         string memory title,
         string memory style,
-        string memory markup,
-        string memory config,
+        string memory body,
+        string memory setting,
         string memory script
     ) public pure returns (string memory) {
+        string[] memory markups = processMarkup();
         string memory encodedMarkup = string(
             abi.encodePacked(
-                "<!DOCTYPE html> <html lang=en> <head> <title>", // byte array[0]
+                markups[0],
                 title,
-                '</title> <meta http-equiv=content-type content="text/html; charset=UTF-8"> <meta charset=UTF-8><meta http-equiv=X-UA-Compatible content="IE=edge"> <meta name=viewport content="width=device-width,initial-scale=1"> <style>', // byte array[2]
+                markups[1],
                 style,
-                "</style></head><body>", // byte array[2]
-                markup,
+                markups[2],
+                body,
                 " ",
-                config,
-                " <script type=module> ", // byte array[3]
+                setting,
+                markups[3],
                 script,
-                " </script> </body> </html>" // byte array[4]
+                markups[4]
             )
         );
         return encodedMarkup;
+    }
+
+    function processMarkup() internal pure returns (string[] memory markups) {
+        markups = new string[](5);
+        markups[0] = decode64(MARKUP1);
+        markups[1] = decode64(MARKUP2);
+        markups[2] = decode64(MARKUP3);
+        markups[3] = decode64(MARKUP4);
+        markups[4] = decode64(MARKUP5);
     }
 }
